c83bed29599a827d00551f63c2a46be931b4af69
mohith-venkata <mohithreddy.venkata@gmail.com>
2022-10-04 14:52:56 +0530

    added CRUD operation

--- routes/api/teachers.js
+++ routes/api/teachers.js
@@ -1,33 +1,15 @@
 const express =  require("express");
 const router = express.Router();
+const teachersController = require('../../controllers/teachersController');
 //const path = require("path");
-const data = {};
-data.teachers = require('../../data/teachers.json');
 
 router.route('/')
-    .get((req, res)=>{
-        res.json(data.teachers);
-    })
-    .post((req,res)=>{
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        })
-    })
-    .put((req,res)=>{
-        res.json({
-            "id": req.body.id,
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        });
-    })
-    .delete((req,res)=>{
-        res.json({"id":req.body.id})
-    })
+    .get(teachersController.getAllTeachers)
+    .post(teachersController.createNewTeacher)
+    .put(teachersController.updateTeacher)
+    .delete(teachersController.deleteTeacher)
 
 router.route('/:id')
-    .get((req,res)=>{
-        res.json({"id": req.params.id});
-    })
+    .get(teachersController.getTeacher)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,24 +5,11 @@ const {logger} = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 app.use(logger);
-
-//cros origin resource sharing  
-const whitelist = ['https://www.yoursite.com','http://127.0.0.1:5500','http://localhost:3500']
-
-const corsOptions = {
-    origin:(origin, callback) => {
-        if(whitelist.indexOf(origin) !== -1 || !origin){  // remove origin in production
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionSuccessStatus: 200
-}
 
 app.use(cors(corsOptions));
 
@@ -34,10 +21,10 @@ app.use(express.json());
 //serve static files
 app.use('/',express.static(path.join(__dirname,'/public')));
 //use static files for subdir also
-app.use('/subdir',express.static(path.join(__dirname,'/public')));
+//app.use('/subdir',express.static(path.join(__dirname,'/public')));
 
 app.use('/',require('./routes/root'));
-app.use('/subdir',require('./routes/subdir'));
+//app.use('/subdir',require('./routes/subdir'));
 app.use('/teachers',require('./routes/api/teachers'));
 
 app.all('*/',(req,res)=>{
